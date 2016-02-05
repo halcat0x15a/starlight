8e3061fa77844736cc6e4897addaf927597aa7a7
Sanshiro Yoshida <halcat0x15a@gmail.com>
2016-02-05 16:29:48 +0900

    refactoring

--- script.js
+++ script.js
@@ -1,121 +1,135 @@
-var HEADERS = ['Name', 'Type', 'Rarity', 'Life', 'Vocal', 'Dance', 'Visual'];
-
-function sort(data, compare) {
-  for (var i = 1; i < data.length; i++) {
-    var tmp = data[i];
-    if (compare(data[i - 1], tmp)) {
-      var j = i;
-      do {
-        data[j] = data[j - 1];
-        --j;
-      } while (j > 0 && compare(data[j - 1], tmp));
-      data[j] = tmp;
+var SortButton = React.createClass({
+  getInitialState: function() {
+    return {order: 'sort'};
+  },
+  sort: function(compare) {
+    var data = this.props.app.state.data;
+    var key = this.props.index;
+    for (var i = 1; i < data.length; i++) {
+      var tmp = data[i];
+      if (compare(data[i - 1][key], tmp[key])) {
+        var j = i;
+        do {
+          data[j] = data[j - 1];
+          --j;
+        } while (j > 0 && compare(data[j - 1][key], tmp[key]));
+        data[j] = tmp;
+      }
     }
+    this.props.app.setState(this.props.app.state);
+  },
+  onClick: function(e) {
+    switch (this.state.order) {
+    case 'sort':
+      this.state.order = 'sort-by-order-alt';
+      this.sort(function(x, y) { return x < y });
+      break;
+    case 'sort-by-order-alt':
+      this.state.order = 'sort-by-order';
+      this.sort(function(x, y) { return x > y });
+      break;
+    case 'sort-by-order':
+      this.state.order = 'sort';
+      this.props.app.loadData();
+      break;
+    }
+    this.setState(this.state);
+  },
+  render: function() {
+    return (
+      <button type="button" className="btn btn-link" onClick={this.onClick}>
+        <span className={"glyphicon glyphicon-" + this.state.order} aria-hidden="true"></span>
+      </button>
+    );
+      
   }
-  return data;
-}
+});
+
+var Checkbox = React.createClass({
+  handleChange: function(e) {
+    this.props.app.setState(this.props.app.state);
+  },
+  predicate: function(col) {
+    return this.refs[col].checked;
+  },
+  label: function(value, i) {
+    return (
+      <label key={i} className="checkbox-inline"><input type="checkbox" ref={value} defaultChecked={true} />{value}</label>
+    );
+  },
+  render: function() {
+    return (
+      <form onChange={this.handleChange}>{this.props.data.map(this.label)}</form>
+    );
+  }
+});
 
 var App = React.createClass({
   getInitialState: function() {
-    return {data: [], order: ['sort', 'sort', 'sort', 'sort', 'sort', 'sort', 'sort'], type: ['Cute', 'Cool', 'Passion'], rarity: ['N', 'R', 'SR', 'SSR']};
+    return {headers: [], data: []};
   },
-  loadCSV: function() {
+  loadData: function() {
     $.ajax({
       url: 'data.tsv',
       cache : false,
       success: function(data) {
-        this.state.data = data.split('\n').map(function(row) {
+        var work = data.split('\n').map(function(row) {
           return row.split('\t').map(function(col) {
             return /\d+/.test(col) ? parseInt(col) : col;
           });
         });
-        console.log(this.state.data);
+        this.state.headers = work[0];
+        this.state.headers.push('Total');
+        this.state.data = work.slice(1).map(function(row) {
+          row.push(row[4] + row[5] + row[6]);
+          return row;
+        });
         this.setState(this.state);
       }.bind(this)
     });
   },
   componentDidMount: function() {
-    this.loadCSV();
+    this.loadData();
   },
-  onClick: function(i) {
-    return function(e) {
-      switch (this.state.order[i]) {
-      case 'sort':
-        this.state.order[i] = 'sort-by-order-alt';
-        sort(this.state.data, function(x, y) { return x[i] < y[i]; });
-        this.setState(this.state);
-        break;
-      case 'sort-by-order-alt':
-        this.state.order[i] = 'sort-by-order';
-        sort(this.state.data, function(x, y) { return x[i] > y[i]; });
-        this.setState(this.state);
-        break;
-      case 'sort-by-order':
-        this.state.order[i] = 'sort';
-        this.loadCSV();
-        break;
-      }
-    }.bind(this);
+  data: function() {
+    return this.state.data.filter(row => this.refs.type.predicate(row[1]) && this.refs.rarity.predicate(row[2]));
   },
-  sortButton: function(i) {
-    return <button type="button" className="btn btn-link" onClick={this.onClick(i)}><span className={"glyphicon glyphicon-" + this.state.order[i]} aria-hidden="true"></span></button>;
+  members: function() {
+    return this.data().sort((x, y) => y[7] - x[7]).slice(0, 5);
   },
   headerColumn: function(col, i) {
-    return <th key={i}>{col}{this.sortButton(i)}</th>;
+    return <th key={i}>{col}<SortButton app={this} index={i} /></th>;
   },
-  handleChange: function(e) {
-    this.state.type = [].concat(
-      this.refs.Cute.checked ? ['Cute'] : [],
-      this.refs.Cool.checked ? ['Cool'] : [],
-      this.refs.Passion.checked ? ['Passion'] : []
-    );
-    this.state.rarity = [].concat(
-      this.refs.N.checked ? ['N'] : [],
-      this.refs.R.checked ? ['R'] : [],
-      this.refs.SR.checked ? ['SR'] : [],
-      this.refs.SSR.checked ? ['SSR'] : []
-    );
-    this.setState(this.state);
+  sumColumn: function(i) {
+    if (i >= 3) {
+      return <td key={i}>{this.members().reduce(function(prev, current) { return current[i] + prev }, 0)}</td>;
+    } else {
+      return <td key={i}></td>;
+    }
   },
   tableRow: function(row, i) {
     return <tr key={i}>{row.map(function(col, j) { return <td key={j}>{col}</td>; })}</tr>;
-  },
-  checked: function(row) {
-    return this.state.type.includes(row[1]) && this.state.rarity.includes(row[2]);
-  },
-  members: function() {
-    return this.state.data.filter(this.checked).sort(function (x, y) { return (y[4] + y[5] + y[6]) - (x[4] + x[5] + x[6]) }).slice(0, 5);
-  },
-  sum: function(i) {
-    return this.members().reduce(function(prev, current) { return current[i] + prev }, 0);
   },
   render: function() {
     return (
       <div className="container-fluid">
         <table className="table table-striped">
           <thead>
-            <tr>{HEADERS.map(function(col, i) { return <th key={i}>{col}</th>; })}</tr>
+            <tr>{this.state.headers.map(function(col, i) { return <th key={i}>{col}</th>; })}</tr>
           </thead>
           <tbody>
             {this.members().map(this.tableRow)}
-            <tr><td></td><td></td><td></td><td>{this.sum(3)}</td><td>{this.sum(4)}</td><td>{this.sum(5)}</td><td>{this.sum(6)}</td></tr>
+            <tr>{this.state.headers.map((col, i) => this.sumColumn(i))}</tr>
           </tbody>
         </table>
-        <form onChange={this.handleChange}>
-          <label className="checkbox-inline"><input type="checkbox" ref="Cute" defaultChecked={true} />Cute</label>
-          <label className="checkbox-inline"><input type="checkbox" ref="Cool" defaultChecked={true} />Cool</label>
-          <label className="checkbox-inline"><input type="checkbox" ref="Passion" defaultChecked={true} />Passion</label>
-          <label className="checkbox-inline"><input type="checkbox" ref="N" defaultChecked={true} />N</label>
-          <label className="checkbox-inline"><input type="checkbox" ref="R" defaultChecked={true} />R</label>
-          <label className="checkbox-inline"><input type="checkbox" ref="SR" defaultChecked={true} />SR</label>
-          <label className="checkbox-inline"><input type="checkbox" ref="SSR" defaultChecked={true} />SSR</label>
-        </form>
+        <Checkbox ref="type" app={this} data={['Cute', 'Cool', 'Passion']} />
+        <Checkbox ref="rarity" app={this} data={['N', 'R', 'SR', 'SSR']} />
         <table className="table table-striped">
           <thead>
-            <tr>{HEADERS.map(this.headerColumn)}</tr>
+            <tr>{this.state.headers.map(this.headerColumn)}</tr>
           </thead>
           <tbody>
-            {this.state.data.filter(this.checked).map(this.tableRow)}
+            {this.data().map(this.tableRow)}
           </tbody>
         </table>
       </div>
